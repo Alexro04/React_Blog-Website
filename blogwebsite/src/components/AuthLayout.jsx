76b223d68ea968731f4223f71bbcef6d4bf14571
Alexro04 <ajakatomi@gmail.com>
2024-11-16 09:18:28 +0100

    Setting up redux and react-router

---
+++ blogwebsite/src/components/AuthLayout.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Protected({children, authentication = true}) {
+
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  let loader = true;
+
+  useEffect(() =>{
+    if (authentication && authStatus !== authentication){
+      navigate("/login");
+    } else if (!authentication && authStatus != authentication) {
+      navigate("/");
+    }
+    loader = false
+  }, [authStatus, authentication, navigate])
+
+  return loader ? null : <>{children}</>
+}
+
+export default Protected
